4ebf9d8cebea538909b0ee91bc8f63f6d0d30c47
Michael Topping <mtopping@astro.ucla.edu>
2015-03-31 16:00:19 -0700

    Now calculates solar system velocity

--- ps1_1.c
+++ ps1_1.c
@@ -1,98 +1,129 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <float.h>
 
-struct Angles{
-	float theta;
-	float phi;
-};
 
-struct Angles sexagesimal(int RAh, int RAm, float RAs, int dech, int decm, float decs);
-float avg(float *arr, int size);
+double RAtoRad(float RAh, float RAm, float RAs);
+double dectoRad(float dech, float decm, float decs, char pm);
 float slope(float *x, float *y, int N);
 float intercept(float *x, float *y, float m, int N);
-
+float regress(float *radii, float *vels, float *RAs, float *decs, int N);
 
 
 //MAIN
 int main()
 {
 
+
 	//Input the data
-	float radii[] = {0.032, 0.034, 0.214, 0.263, 0.275, 0.275, 0.45, 0.5, 0.5, 0.63, 0.8, 0.9, 0.9, 0.9, 0.9, 1.0, 1.1, 1.1, 1.4, 1.7, 2.0, 2.0, 2.0, 2.0};
-	float vels[] = {170, 290, -130, -70, -185, -220, 200, 290, 270, 200, 300, -30, 650, 150, 500 ,920, 450, 500, 500, 960, 500, 850, 800, 1090};
-	float mags[] = {1.5, 0.5, 9.0};
-	float absMags[] = {-16.0, -17.2, -12.7};
 	int N = 24;
+	float radii[N];
+	float vels[N];
 
-	struct Angles angles = sexagesimal(0, 51, 0, -73, 6, 0);
+	float RAarr[24];
+	float decArr[24];
 
-	float m = slope(radii, vels, N);
-	float b = intercept(radii, vels, m, N);
-	printf("Slope of: %f\n", m);
-	printf("Intercept of: %f\n", b);
+	FILE *inputFile;
+	
+	inputFile = fopen("Hub_1929.dat", "r");
+	
+	float r, v, m, M;
+	char ID[5];
+	float RAh, RAm, RAs;
+	float dech, decm, decs;
+	char h, min, s;
+	char pm;
+	char dec[11];
+	
+	for (int ii=0; ii<N; ii++) {
+		fscanf(inputFile, "%f %f %f %f %s %f %c %f %c %f %c %c %f %c %f %c %f %c", &r, &v, &m, &M, ID, &RAh, &h, &RAm, &min, &RAs, &s, &pm, &dech, &h, &decm, &min, &decs, &s);
+		//printf("%f %f %f %f %s %f %c %f %c %f %c %c %f %c %f %c %f %c", r, v, m, M, ID, RAh, h, RAm, min, RAs, s, pm, dech, h, decm, min, decs, s);
+		radii[ii] = r;
+		vels[ii] = v;
+		RAarr[ii] = RAtoRad(RAh, RAm, RAs);
+		decArr[ii] = dectoRad(dech, decm, decs, pm);
+		
+	}
+	fclose(inputFile);
+
+	//Hr + Xcosαcosδ + Ysinαcosδ + Zsinδ
+	regress(radii, vels, RAarr, decArr, N);
+
+	
 	return 0;
 }
 
 
 
-//gets RA and dec and returns angles to the object
-struct Angles sexagesimal(int RAh, int RAm, float RAs, int dech, int decm, float decs) 
+double RAtoRad(float RAh, float RAm, float RAs)
 {
 	float RAdeci;
+	RAdeci = RAh * 15 + RAm * (15.0 / 60.0) + RAs * (15.0 / 3600);
+	return (RAdeci * M_PI / 180.0);
+
+}
+
+double dectoRad(float dech, float decm, float decs, char pm)
+{
 	float decdeci;
 	
-	RAdeci = RAh * 15 + RAm * (15.0 / 60.0) + RAs * (15.0 / 3600);
 	decdeci = abs(dech) + decm  / 60.0 + decs  / 3600.0;
-	if (dech < 0) {
+	if (pm == '-') {
 		decdeci = -decdeci;
 	}
 	
-	struct Angles angles;
-	angles.theta = decdeci;
-	angles.phi = RAdeci;
+	return (decdeci * M_PI / 180.0);
+}
+
+
+
+float regress(float *radii, float *vels, float *RAs, float *decs, int N)
+{
+	float Rsqr = 0;
+	float minRes= DBL_MAX;
+	float Hmax = 500;
+	float Xmax = 100;
+	float Ymax = 275;
+	float Zmax = 250;
+	float Hmin = 0;
+	float Xmin = 0;
+	float Ymin = 0;
+	float Zmin = 0;
 	
-	return angles;
-}
+	//loop through all possible values
+	//H ~ 465
+	//X ~ -70
+	//Y ~ 240
+	//Z ~ -200
+	for(float H = 400.0; H < Hmax; H += 1) {
+		printf("H is now: %f\n", H);
+		for(float X = -Xmax; X < 0; X += 1) {
+			//printf("X is now: %f\n", X);
 
+			for(float Y = 200; Y < Ymax; Y += 1) {
+				//printf("Y is now: %f\n", Y);
 
+				for(float Z = -Zmax; Z < -150; Z += 1) {
+					Rsqr = 0;
+					for(int ii = 0; ii < N; ii++) {
 
-//calculate the average of a list of numbers
-float avg(float *arr, int size)
-{
-	float total = 0;
-	for (int ii = 0; ii < size; ii++) {
-		total += arr[ii];
+						Rsqr += pow((vels[ii] - H*radii[ii] - X*cos(RAs[ii])*cos(decs[ii]) - Y*sin(RAs[ii])*cos(decs[ii]) - Z*sin(decs[ii])),2);
+					
+					}
+					if(Rsqr < minRes) {
+						minRes = Rsqr;
+						Hmin = H;
+						Xmin = X;
+						Ymin = Y;
+						Zmin = Z;
+					}
+				}
+			}
+		}
 	}
-	float average = total/size;
-	return average;
+	printf("minRes: %f with H, X, Y, Z: %f %f %f %f\n",minRes, Hmin, Xmin, Ymin, Zmin);
+	return 0.0;
 }
 
 
-//calculate the slope from two arrays
-float slope(float *x, float *y, int N)
-{
-	//create an array of x*y
-	float xy[N];
-	for (int ii = 0; ii < N; ii ++) {
-		xy[ii] = x[ii]*y[ii];
-	}
-
-	//create an array of x**2
-	float xx[N];
-	for (int ii = 0; ii < N; ii ++) {
-		xx[ii] = x[ii]*x[ii];
-	}
-
-	//this gives the slope m
-	float m = (avg(xy, N) - avg(x,N)*avg(y,N)) / (avg(xx,N) - avg(x,N)*avg(x,N));
-	
-	return m;
-}
-
-
-//calculate the y-intercept from two arrays and the least squares regression slope
-float intercept(float *x, float *y, float m, int N)
-{
-	float b = avg(y,N) - m*avg(x,N);
-	return b;
-}
